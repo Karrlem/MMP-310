ed38584ff1e1379805c373216e73c3b4b275a3d9
Karrlem Lewis <karrlemjr.lewis@f1006-22-34176.bmcc.cuny.edu>
2016-02-10 19:07:47 -0500

    worked on Assignment 2 a little

--- Week 2/Assignment 2/app.js
+++ Week 2/Assignment 2/app.js
@@ -2,11 +2,12 @@ var submit = document.getElementById("submit");
 
 submit.onclick = function() {
 	var name = document.getElementById("name").value;
+	var birth = document.getElementById("birth").value;
 	var genres = document.getElementById("genres").value;
 	var message = document.getElementById("message");
-	console.log(name, genres);
+	console.log(name, birth);
 
-	message.innerHTML = " Hello, my name is " + name  
-	 +  " my favorite genre is: " + genres;
+	message.innerHTML = " Hello, my name and gamertag is " + name  
+	 +  " my hometown and birthday is " + birth;
 	
 };
