1717f022ec747d94646b91cbe2e38c9dccc3f26b
Karrlem Lewis <karrlemjr.lewis@f1006-22-34176.bmcc.cuny.edu>
2016-02-17 18:46:56 -0500

    Assignment 3 finished

--- Week 3/Assignment 3/app.js
+++ Week 3/Assignment 3/app.js
@@ -2,35 +2,21 @@ var submit = document.getElementById("submit");
 
 submit.onclick = function() {
 
-	var rname = document.getElementById("rname").value;
-	var gtname = document.getElementById("gtname").value;
-	var birthTown = document.getElementById("birthTown").value;
-	var birthDay = document.getElementById("birthDay").value;
-	var firstGame = document.getElementById("firstGame").value;
+	var answers = [];
+
+	var answerElements = document.getElementsByClassName("answer");
+
+	for (var i = 0; i < answerElements.length; i++)
+		answers.push(answerElements[i].value);
+
 	var message = document.getElementById("message");
 
-	var genres = document.getElementById("genres").value;
-	var gamerType = document.getElementById("gamerType").value;
-	var console = document.getElementById("console").value;
-	var store = document.getElementById("store").value;
-	var hours = document.getElementById("hours").value;
-
-	//var responses = [];
-	//var responseElements = document.getElementsByClassName("response");
-	//console.log(responseElements);
-
-	//for (var i = 0; i < responseElements.length; i++)
-	//responses/push(responseElements[i].value);
-
-	//var message = document.getElementById("message");
-
-
-	message.innerHTML = " Hello, my name is " + rname + 
-	", my gamertag is. " + gtname +  " I was born in " + birthTown + 
-	" on, " + birthDay + " and my first game was " + firstGame +
-	". My favorite game genre is " + genres + 
-	", the kind of gamer that I am is a " + gamerType + " gamer. " +
-	" My preferred console is a " + console + " I buy my games " +
-	store + ". I spend " + hours + " a week playing games. ";
+	message.innerHTML = " Hello, my name is " + answers[0] + 
+	", my gamertag is. " + answers[1] +  " I was born in " + answers[2] + 
+	" on, " + answers[3] + " and my first game was " + answers[4] +
+	". My favorite game genre is " + answers[5] + 
+	", the kind of gamer that I am is a " + answers[6] + " gamer. " +
+	" My preferred console is a " + answers[7] + " I buy my games " +
+	answers[8] + ". I spend " + answers[9] + " a week playing games. ";
 	
 };
