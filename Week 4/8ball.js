f088b489b5cdff6a9ddf05ddac35fe05e05324cf
Karrlem Lewis <karrlemjr.lewis@f1006-22-34176.bmcc.cuny.edu>
2016-02-29 19:13:20 -0500

    Updated Homework

--- Week 4/8ball.js
+++ Week 4/8ball.js
@@ -1,50 +1,9 @@
+var submit = document.getElementById("submit");
 
+submit.onclick = function() {
+var question = document.getElementById("question").value;
+    
+ message.innerHTML = a[Math.floor(Math.random() *a.length)]
+};
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//var messages = new Array();
-//messages[0] = "No";
-//messages[1] = "Not today";
-//messages[2] = "It is decidedly so";
-//messages[3] = "Without a doubt";
-//messages[4] = "Yes definitely";
-//messages[5] = "You may rely on it";
-//messages[6] = "As I see it yes";
-//messages[7] = "Most likely";
-//messages[8] = "Outlook good";
-//messages[10] = "Signs point to yes";
-//messages[11] = "Reply hazy try again";
-//messages[12] = "Ask again later";
-//messages[13] = "Better not tell you now";
-//messages[14] = "Cannot predict now";
-//messages[15] = "Concentrate and ask again";
-//messages[16] = "Don't count on it";
-//messages[17] = "My reply is no";
-//messages[18] = "My sources say no";
-//messages[19] = "Outlook not so good";
-//messages[20] = "Very doubtful";
-//
-//result = function () {
-//    var randomnumber = Math.floor(Math.random() * 21);
-//    document.getElementById("res").innerHTML = "Answer:  " + messages[randomnumber];
-//}
-//
-//$('#bcirc').click(function () {
-//    $('.shake').effect("shake", {
-//        times: 8,
-//        direction: "up"
-//    }, 2000);
-//    var mytimer = window.setTimeout('result()', 3000);
-//});
+var a  = ["It is certain", "It is decidedly so", "Without a doubt",  "Yes, definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again",  "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"];
