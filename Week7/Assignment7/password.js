067122bc0d84da63b20a6884241e2af2882f21f4
Karrlem Lewis <karrlemjr.lewis@f1006-22-34176.bmcc.cuny.edu>
2016-03-21 19:20:25 -0400

    Assignment 7

--- Week7/Assignment7/password.js
+++ Week7/Assignment7/password.js
@@ -1,8 +1,15 @@
-var usernames = ["Adam","Bob","Jack"];
+//36 array options
+var pOptions = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","1","2","3","4","5","6","7","8","9","0"];
 
-var passwords = ["abc", "wasd", "efgh"];
+var pass =''
 
-var makePasswords = function(){
+function passwordGenerator(plength){
+pass=''
+for (var i = 0;i < plength; i++)
+pass+=pOptions.charAt(Math.floor(Math.random() * pOptions.length))
+return pass
+}
 
-
+function populateform(enterlength){
+document.passGen.output.value=passwordGenerator(enterlength)
 }
