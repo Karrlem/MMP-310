90650380113ac47bdf835d2fdb061bbe3e3d003a
Karrlem <karrlemlewis@yahoo.com>
2016-03-23 13:58:55 -0400

    finished assignment 7

--- Week7/Assignment7/password.js
+++ Week7/Assignment7/password.js
@@ -1,5 +1,5 @@
 //36 array options
-var pOptions = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","1","2","3","4","5","6","7","8","9","0"];
+var pOptions ="abcdefghijklmnopqrstuvwxyz123456789";
 
 var pass =''
 
