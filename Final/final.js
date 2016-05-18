2f36c2dcfda967510e06e0aeaaaa8baa39e14931
Karrlem Lewis <karrlemjr.lewis@f1006-22-34176.bmcc.cuny.edu>
2016-05-18 19:22:49 -0400

    worked on final

--- Final/final.js
+++ Final/final.js
@@ -1,3 +1,4 @@
+//Global Variables
 var bullets;
 var symbols;
 var player;
@@ -9,14 +10,18 @@ var hitEnemy;
 var die;
 var MARGIN = 40;
 var submit = document.getElementById("submit");
-var input, button;
+var button;
+var gameover = false;
 
-//var randomSprite;
+var score = 0;
+var playerdead = false;
+
+//Preload music
 function preload(){
 	stage = loadSound('sounds/stage.mp3');
-
-
 }
+
+//setup the game
 function setup(){
 createCanvas(800,600);
 bg = loadImage("images/MMP310FinalBackground.png");
@@ -29,14 +34,7 @@ hit = loadSound('sounds/hit.mp3');
 stage.setVolume(0.4);
 stage.loop();
 
-input = createInput();
-input.position(20,65);
-
-button = createButton('submit');
-button.position(150, 65);
-button.mousePressed(restart);
-
-
+//player information
 player = createSprite(width/2, height/2);
 
 player.addImage("normal",playerImage);
@@ -48,12 +46,12 @@ player.debug = true;
 enemies = new Group();
 bullets = new Group();
 
+//loop that controls the number of enemies that spawn at the start
 for(var i = 0; i<4; i++) {
 	enemySpawn();
+	}
 }
-
-//	randomSprite = createSprite(500, 500, 20, 20);
-}
+var a, textx, texty;
 
 function draw(){
 background(bg);
@@ -65,7 +63,15 @@ fill(0);
 text("Karrlem Lewis",153, 80);
 text("MMP310 Final",153,106);
 text("Mastroids", 133, 132);
+text("Score:" + score, width/2,55);
 
+
+fill(255,182,193,a);
+a -= 10;
+text("100+", textx, texty);
+texty--;
+
+//loop that controls the edge of the canvas
 for(var i=0; i< allSprites.length; i++){
   var s = allSprites[i];
   if(s.position.x<-MARGIN) s.position.x = width+MARGIN;
@@ -74,21 +80,27 @@ for(var i=0; i< allSprites.length; i++){
   if(s.position.y>height+MARGIN) s.position.y = -MARGIN;
 }
 
+//enemy collision with bullet and player collision with enemy
 enemies.overlap(bullets,enemyHit);
 enemies.forEach( function(enemy)  {
 	enemy.move();
 });
 player.overlap(enemies,playerHit);
 
-if(keyDown(LEFT_ARROW))
+//Player movement
+if(keyDown(LEFT_ARROW) && !playerdead)
 	player.rotation -= 4;
-if(keyDown(RIGHT_ARROW))
+if(keyDown(RIGHT_ARROW) && !playerdead)
 	player.rotation += 4;
-if(keyDown(UP_ARROW)){
+if(keyDown(UP_ARROW) && !playerdead){
 	player.addSpeed(.2,player.rotation);
 }
+if(keyDown(DOWN_ARROW) && !playerdead){
+	player.addSpeed(-.2,player.rotation);
+}
 
-if(keyWentDown("space")){
+//Shooting
+if(keyWentDown("space") && !playerdead){
 	var bullet = createSprite(player.position.x,player.position.y);
 	bullet.addImage("normal", bulletImage);
 	bullet.setSpeed(10+player.getSpeed(), player.rotation);
@@ -101,6 +113,7 @@ if(keyWentDown("space")){
 	drawSprites();
 }
 
+//creates enemy
 function createEnemy(type, x, y){
 	var e = createSprite(x,y);
 	var img = loadImage("images/enemy"+ floor(random(0,4))+".png");
@@ -136,18 +149,34 @@ function createEnemy(type, x, y){
 }
 function enemyHit(enemy, bullet){
 bullet.remove();
+	textx = enemy.position.x;
+	texty = enemy.position.y;
+	a = 255;
 enemy.remove();
 enemySpawn();
-score +10;
-
+	score += 100;
 }
 
 
 function playerHit(){
+playerdead = true;
 player.remove();
 hit.play();
-}
 
+		var button = createButton('GameOver');
+		button.position(width/2-button.width,height/2);
+	button.mousePressed(gameOver);
+	button.style("font-size", "40px");
+	button.style("color", "#f44336");
+	button.style("background-color", "#FFFFFF");
+	button.style("border-color", "#ADD8E6");
+	button.style("box-shadow", "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)");
+	button.style("hover","background-color: #008CBA;");
+//	text("Click game over to try again",width/2,height/2);
+
+	}
+
+//enemy spawner
 function enemySpawn(){
   var ang = random(360);
   var px = width/2 + 1000 * cos(radians(ang));
@@ -156,6 +185,7 @@ function enemySpawn(){
 }
 setInterval(enemySpawn, 10000);
 
-function restart(){
+
+function gameOver(){
 	window.location.reload();
 }
